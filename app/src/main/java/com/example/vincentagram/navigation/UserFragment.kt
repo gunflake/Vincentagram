060c2cb474874c3b624f7e68872b60a3dce555b7
Vincent <gunflake09@gmail.com>
2020-01-12 00:17:44 +0900

    유저 Follow, Following 숫자 기능 및 버튼 클릭시 동작 구현

--- app/src/main/java/com/example/vincentagram/navigation/UserFragment.kt
+++ app/src/main/java/com/example/vincentagram/navigation/UserFragment.kt
@@ -16,6 +16,7 @@ import com.example.vincentagram.LoginActivity
 import com.example.vincentagram.MainActivity
 import com.example.vincentagram.R
 import com.example.vincentagram.navigation.model.ContentDTO
+import com.example.vincentagram.navigation.model.FollowDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
@@ -46,6 +47,8 @@ class UserFragment : Fragment() {
         firestore = FirebaseFirestore.getInstance()
         curerntUseruid = FirebaseAuth.getInstance().currentUser?.uid
 
+        getFollowAndFollowing()
+
         if (uid == curerntUseruid) { //My Page
             fragmentView?.account_btn_follow_signout?.text = getString(R.string.signout)
             fragmentView?.account_btn_follow_signout?.setOnClickListener {
@@ -54,7 +57,11 @@ class UserFragment : Fragment() {
                 startActivity(Intent(activity, LoginActivity::class.java))
             }
         } else { // Other User Page
-            fragmentView?.account_btn_follow_signout?.text = getString(R.string.follow)
+
+            //Follow, Following 버튼을 클릭할 떄마다 해당 기능에 맞는 동작
+            fragmentView?.account_btn_follow_signout?.setOnClickListener{
+                requestFollow()
+            }
             var mainActivity = (activity as MainActivity)
 
             mainActivity.toolbar_username.text = arguments?.getString("destinationUserId")
@@ -83,16 +90,102 @@ class UserFragment : Fragment() {
         return fragmentView
     }
 
-    private fun getProfileImage() {
-        FirebaseFirestore.getInstance().collection("profileImages").document(uid!!).addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
-            if(documentSnapshot == null)
+    private fun getFollowAndFollowing() {
+        firestore?.collection("users")?.document(uid!!)?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+            if(documentSnapshot == null){
                 return@addSnapshotListener
+            }
 
-            if(documentSnapshot.data != null){
-                var url = documentSnapshot.data!!["images"]
-                Glide.with(activity!!).load(url).apply(RequestOptions().circleCrop()).into(fragmentView?.account_iv_profile!!)
+            var followDTO = documentSnapshot.toObject(FollowDTO::class.java)
+
+            if(followDTO?.followingCount != null){
+                fragmentView?.account_tv_following_count?.text = followDTO.followingCount.toString()
+            }
+            if(followDTO?.followerCount != null){
+                fragmentView?.account_tv_follow_count?.text = followDTO.followerCount.toString()
+
+                if(curerntUseruid!=uid){
+                    if (followDTO.followers.containsKey(curerntUseruid)) {
+                        fragmentView?.account_btn_follow_signout?.text = "Following"
+                    } else {
+                        fragmentView?.account_btn_follow_signout?.text = "Follow"
+                    }
+                }
             }
         }
+    }
+
+    // Following 기능
+    private fun requestFollow() {
+        // save data to my account
+        var tsDocFollowing = firestore?.collection("users")?.document(curerntUseruid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollowing!!).toObject(FollowDTO::class.java)
+
+            // 내 계정에 연결된 follow가 한명도 없다면...
+            if (followDTO == null) {
+                followDTO = FollowDTO()
+                followDTO!!.followingCount = 1
+                // TODO : 확인해보기 followers 인지 followings 인지
+                followDTO!!.followings[uid!!] = true
+
+                transaction.set(tsDocFollowing, followDTO)
+                return@runTransaction
+            }
+
+            // 내가 상대방을 이미 팔라우하고 있다면
+            if(followDTO.followings.containsKey(uid)){
+                followDTO.followingCount -= 1
+                followDTO.followings.remove(uid)
+            }else{ // 내가 상대방을 팔라우하고 있지 않으면
+                followDTO.followingCount += 1
+                followDTO.followings[uid!!] = true
+            }
+            transaction.set(tsDocFollowing, followDTO)
+            return@runTransaction
+        }
+
+        // Save to following Account
+        var tsDocFollow = firestore?.collection("users")?.document(uid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollow!!).toObject(FollowDTO::class.java)
+
+            //상대방을 팔로우 하는 사람이 내가 처음이라면...
+            if(followDTO == null){
+                followDTO = FollowDTO()
+                followDTO!!.followerCount = 1
+                followDTO!!.followers[curerntUseruid!!] = true
+
+                transaction.set(tsDocFollow, followDTO!!)
+                return@runTransaction
+            }
+
+            //상대방을 이미 팔로우하고 있다면
+            if(followDTO!!.followers.containsKey(curerntUseruid)){
+                followDTO!!.followerCount -= 1
+                followDTO!!.followers.remove(curerntUseruid)
+            }else{
+                followDTO!!.followerCount += 1
+                followDTO!!.followers[curerntUseruid!!] = true
+            }
+
+            transaction.set(tsDocFollow, followDTO!!)
+            return@runTransaction
+        }
+    }
+
+    private fun getProfileImage() {
+        FirebaseFirestore.getInstance().collection("profileImages").document(uid!!)
+            .addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                if (documentSnapshot == null)
+                    return@addSnapshotListener
+
+                if (documentSnapshot.data != null) {
+                    var url = documentSnapshot.data!!["images"]
+                    Glide.with(activity!!).load(url).apply(RequestOptions().circleCrop())
+                        .into(fragmentView?.account_iv_profile!!)
+                }
+            }
 
     }
 
