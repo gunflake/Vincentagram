02a2bc0f2abd1611936468c59996c82f301e212a
Vincent <gunflake09@gmail.com>
2020-01-12 00:17:58 +0900

    Gird 격자 무늬 레이아웃 구현

--- app/src/main/java/com/example/vincentagram/navigation/GridFragment.kt
+++ app/src/main/java/com/example/vincentagram/navigation/GridFragment.kt
@@ -4,18 +4,77 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.vincentagram.R
+import com.example.vincentagram.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
-class GridFragment : Fragment(){
+class GridFragment : Fragment() {
+
+    var firestore: FirebaseFirestore? = null
+    var fragmentView: View? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid, container, false)
-        return view
+        fragmentView =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_grid, container, false)
+        firestore = FirebaseFirestore.getInstance()
+
+        fragmentView?.grid_recyclerView?.adapter = GridFragmentRecyclerView()
+        fragmentView?.grid_recyclerView?.layoutManager = GridLayoutManager(activity, 3)
+
+        return fragmentView
+    }
+
+    inner class GridFragmentRecyclerView : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.orderBy("timestamp", Query.Direction.DESCENDING)
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    if (querySnapshot == null) {
+                        return@addSnapshotListener
+                    }
+
+                    for (snapshot in querySnapshot) {
+                        contentDTOs.add(snapshot.toObject(ContentDTO::class.java))
+                    }
+
+                    notifyDataSetChanged()
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+
+            var imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView) : RecyclerView.ViewHolder(imageView)
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageView = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl)
+                .apply(RequestOptions().centerCrop()).into(imageView)
+        }
+
     }
 
 }
